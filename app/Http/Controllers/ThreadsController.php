74548973dc7ee7d731dcdd69f952251f071f9a6b
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-03-02 10:11:44 -0500

    Creatin Models and Migrations

---
+++ app/Http/Controllers/ThreadsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Thread;
+use Illuminate\Http\Request;
+
+class ThreadsController extends Controller
+{
+
+}
---
+++ notes/log.md
@@ -0,0 +1,7 @@
+## Lets-Build-A-Forum-with-Laravel-and-TDD
+
+### Lesson01:
+#### 1.1 create Model and controller for Thread and Reply
+
+
+ 
