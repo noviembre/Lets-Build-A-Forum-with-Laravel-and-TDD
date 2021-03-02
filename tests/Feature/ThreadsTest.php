25070d8527d9f9715c502173653b564f89849d37
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-03-02 11:48:36 -0500

    a Usr can vie a single thread

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -21,4 +21,13 @@ class ThreadsTest extends TestCase
         $response = $this->get('/threads/' . $thread->id);
         $response->assertSee($thread->title);
     }
+
+    /** @test */
+    function a_user_can_view_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+    }
 }
