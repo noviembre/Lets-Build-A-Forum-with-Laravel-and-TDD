71367b4a36297634b6e0cf81f009aebaa6765d8f
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-03-04 09:40:40 -0500

    4.1 Partial fial for reply

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -17,24 +17,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 @foreach($thread->replies as $reply)
-                    <div class="card">
-
-                        <div class="card-header">
-                            <a href="#">
-                                {{ $reply->owner->name }}
-                            </a>
-
-                            said {{ $reply->created_at->diffForHumans() }}
-
-                        </div>
-                        <div class="card-body">
-                            {{ $reply->body }}
-                        </div>
-                        <div class="card-footer">
-
-                        </div>
-                    </div>
-                    <br>
+                    @include('threads.reply')
                 @endforeach
 
 
