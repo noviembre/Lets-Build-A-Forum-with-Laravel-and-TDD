f7d8ec3660909ba49a0d441f58266e2fabc3c960
Babo Elementary PC 2021 <perudragon@hotmail.com>
2021-03-04 09:51:40 -0500

    Relationship Thread-User

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -16,4 +16,11 @@ class ThreadTest extends TestCase
         $thread = factory('App\Thread')->create();
         $this->assertInstanceOf('Illuminate\Database\Eloquent\Collection', $thread->replies);
     }
+
+    /** @test */
+    function a_thread_has_a_creator()
+    {
+        $thread = factory('App\Thread')->create();
+        $this->assertInstanceOf('App\User', $thread->user);
+    }
 }
